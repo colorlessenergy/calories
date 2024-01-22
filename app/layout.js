6b61bd0eb7c34be5994999af7b6bf20118ebaee2
colorlessenergy <brianmunozdev@gmail.com>
2024-01-21 19:50:27 -0800

    add poppins font

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,7 @@
+import { Poppins } from 'next/font/google';
 import './globals.css';
+
+const poppins = Poppins({ subsets: ['latin'], weight: ['400', '700'] });
 
 export const metadata = {
     title: 'calories',
@@ -8,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <body>{children}</body>
+            <body className={poppins.className}>{children}</body>
         </html>
     );
 }
